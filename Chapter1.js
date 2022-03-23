f84b00e8d242dabae5c1a0460a3126f93374b211
Joshua <contact@joshuaschlueter.com>
2022-03-23 15:50:48 -0500

    start ch3

---
+++ Chapter1.js
@@ -0,0 +1,9 @@
+let numbers = 123
+let fractionalNumbers = 12.3
+let bigNumbers = 12e3
+console.log(100 + 4 * 5);
+console.log((100 + 4) * 5);
+console.log(2.5 + 3.5);
+let stringA = "This is the first line. \nAnd this is the second. We use backslash to escape";
+let stringB = `This is first
+this is second`;
---
+++ Chapter3/ch3.js
@@ -0,0 +1,10 @@
+//#FUNCTIONS# programming is a wall of mini stones.
+
+const functionDefinition = function(parameter) /*This is a function expression */ {
+    //the body of the function
+    // if you have no return value the function only produces SIDE EFFECTS
+    return `your argument here: ${parameter}`
+};
+functionDefinition("Argument");
+
+// on scope, 
