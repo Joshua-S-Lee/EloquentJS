2a5b9edaf4bd04df30d10a2a568f13914ef395f2
Joshua <contact@joshuaschlueter.com>
2022-03-23 13:54:17 -0500

    add ch1 notes, add fizzbuzz

--- Chapter2/Fizzbuzz.js
+++ Chapter2/Fizzbuzz.js
@@ -1,6 +1,20 @@
 function fizzBuzz(){
-    let counter = 0
-    while (counter <= 100) {
-        console.log(counter)
-    }
-}
+    let counter = 0;
+    while (counter < 100) {
+        ++counter;
+        if (counter % 3 === 0 && counter % 5 === 0){
+            console.log("FizzBuzz")
+        } else if (counter % 3 === 0) {
+            console.log("Fizz")
+        } else if (counter % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(counter)
+        };
+        
+
+    };
+};
+
+fizzBuzz();
+// make it dynamic by including a target parameter and swapping 100 for target. 
--- Chapter2/LoopingAPyramid.js
+++ Chapter2/LoopingAPyramid.js
@@ -1,14 +1,3 @@
-let numbers = 123
-let fractionalNumbers = 12.3
-let bigNumbers = 12e3
-console.log(100 + 4 * 5);
-console.log((100 + 4) * 5);
-console.log(2.5 + 3.5);
-let stringA = "This is the first line. \nAnd this is the second. We use backslash to escape";
-let stringB = `This is first
-this is second`;
-
-
 function pyramidMaker(height) {
     let block = ""
     for (let w =0 ; w < height; w++){
