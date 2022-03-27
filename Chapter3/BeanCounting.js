1c74bfa618ea779a967a58f4fc81543024bca446
Joshua Schlueter <contact@joshuaschlueter.com>
2022-03-26 21:27:08 -0500

    bean counting

--- Chapter3/BeanCounting.js
+++ Chapter3/BeanCounting.js
@@ -1,3 +1,9 @@
 // write a function that only accepts a string and returns the number of "B"'s 
 // write a countChar that takes a second argument to indicate the target counter, then incorporate into countB's
 // hint string chars can be accessed by "string"[0]
+
+function bCounter() {
+
+}
+
+console.log(bCounter("Billy better than Brett."))
