f6c380859e9614997612fd44dd41368ff12bfd7c
Joshua Schlueter <contact@joshuaschlueter.com>
2022-03-28 12:14:01 -0500

    bean counting final

--- Chapter3/BeanCounting.js
+++ Chapter3/BeanCounting.js
@@ -2,10 +2,10 @@
 // write a countChar that takes a second argument to indicate the target counter, then incorporate into countB's
 // string chars can be accessed by "string"[0]
 
-function bCounter(word) {
+function bCounter(word, target) {
     let count = 0
     for (let i = 0; i < word.length -1; i++)  {
-        if (word[i] === "B") {
+        if (word[i] === target) {
             count++
         };
 
@@ -13,5 +13,5 @@ function bCounter(word) {
     return count
 }
 
-console.log(bCounter("Billy better than Brett."))
-console.log(bCounter("Never buy Boots."))
+console.log(bCounter("Billy better than Brett.", "a"))
+console.log(bCounter("Never buy Boots.", "u"))
