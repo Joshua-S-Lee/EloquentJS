c29cfbe7d80a021e0ed7514e899d8f37be03d066
Joshua <contact@joshuaschlueter.com>
2022-03-29 21:00:19 -0500

    idk

---
+++ Chapter4/aList.js
@@ -0,0 +1,22 @@
+// build a function that runs an array into a list
+function arrayToList(arr){
+
+}
+
+//build a function that returns an array from a list
+function listToArray(list){
+
+}
+
+// write a function to add a new item to the beginning of a list
+function prepend(list,newLink){
+
+}
+
+//write a funtion to return the value at nth of a list, make it recursive
+function findN(list,target){
+
+    if (list[target]==undefined){
+        return undefined
+    }
+}
---
+++ Chapter4/deepComparison.js
@@ -0,0 +1,6 @@
+// write a function that checks strict equality, objects should have a recursive call of deepEqual to compare each objects properties
+// Note: null.typeof returns object, Object.keys will be helpful when comparing object properties.
+
+function deepEqual(alpha, bravo){
+    
+}
