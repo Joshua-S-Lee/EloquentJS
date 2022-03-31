69b73af4f00ac2b8e146e90e2155b0abc2241c60
Joshua Schlueter <contact@joshuaschlueter.com>
2022-03-31 12:44:36 -0500

    arrToList

--- Chapter4/aList.js
+++ Chapter4/aList.js
@@ -1,8 +1,17 @@
 // build a function that runs an array into a list
-function arrayToList(arr){
+//take an array and for each index make and object that stores the index:value pair and object-node holding the next index:value and node.
+//first create and empty list-object, then loop through the array mapping recursively
+function arrayToList(array){
+    let list = {};
+    for (index of array){
+        if (index != undefined){
+            list[index] = array[index];
 
-}
-
+        }
+    };
+    return list
+};
+console.log(arrayToList([2,4,6,8]));
 //build a function that returns an array from a list
 function listToArray(list){
 
@@ -20,3 +29,4 @@ function findN(list,target){
         return undefined
     }
 }
+
